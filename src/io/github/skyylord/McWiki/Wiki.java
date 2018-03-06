7a69cd61e0d638c68976054d81c4f7a5cbad3b80
skylerdev <skyler2@mac.com>
2018-03-05 16:49:45 -0800

    overhaul init

--- src/io/github/skyylord/McWiki/Wiki.java
+++ src/io/github/skyylord/McWiki/Wiki.java
@@ -1,40 +1,69 @@
 package io.github.skyylord.McWiki;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.bukkit.command.Command;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+
 public class Wiki extends JavaPlugin{
+    
+	private static final Logger LOGGER = Logger.getLogger("McWiki");
 	
 	//config setup:
 	FileConfiguration config = getConfig(); 
+	
+	String lang = "en";
+	
 	
 	//on enable:
 	  @Override
 	    public void onEnable() {
 		  //build config 
 		  
-		  config.addDefault("onFailure", "Links you to the wiki.");
-		  config.addDefault("usageMessage", "/wiki [article]");
-		  config.addDefault("messageToSend", "§b{article}§b: §o{link}");
+		  config.addDefault("languge", "en");
+		  config.addDefault("languge", "en");
+		  config.addDefault("languge", "en");
 		  config.options().copyDefaults(true);
 		  saveConfig();
 		 
+		  //load config 
+		  String clang = config.getString("language");
+		  
+		  if(clang.length() != 2) {
+			  LOGGER.log(Level.WARNING, "The language file has been configured incorrectly, using english.");
+		  }else {
+			  lang = clang;
+		  }
+		  
 		  
 	    }
+	  
+	  
 	    //on disable:
 	    @Override
 	    public void onDisable() {
-	    	//cleanup "
+	    	   
 
 	    }
 	    
+	    
+	    //command
 	    @Override
 	    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	    	if (cmd.getName().equalsIgnoreCase("wiki")) { 
 	    		if(args.length == 0){
-	    			sender.sendMessage(config.getString("onFailure"));
-	    			sender.sendMessage(config.getString("usageMessage"));
 	    			return true; 
 	    		}
 	    		String article = "";
@@ -46,20 +75,39 @@ public class Wiki extends JavaPlugin{
 	    			 article = article + args[i] + "_";
 	    			}
 	    		}
-	    		sender.sendMessage(wikiMessage(article));
+	   
+	    		 String url = "www.minecraft.gamepedia.com/" + article;
+	    		 
+	    		 Document doc = new Document("notfound");
+				try {
+					doc = Jsoup.connect(url).get();
+					LOGGER.log(Level.INFO, "Connected to " + url);
+				} catch (IOException e1) {
+					LOGGER.log(Level.INFO, "Failed connection to " + url);
+					e1.printStackTrace();
+					return true;
+				}				
+			
+				LOGGER.log(Level.INFO, doc.title());
+				//Checks if the article is valid by checking if bold text is present in one of the paragraphs
+				
+				 Elements paragraphs = doc.select("p");
+			        for(Element p : paragraphs) {
+			            if(p.selectFirst("b") != null) {
+			           System.out.println(p.toString());
+			           return true;
+			            }
+			        }
+				
+			
+			
+	    		
+	    		
+	    		
 	    		return true;
 	    		}
 	    	return false;
 	    	
-	    }
-	    
-	    private String wikiMessage(String article){
-	    	String currentMessage = config.getString("messageToSend");
-	    	currentMessage.replaceAll("(\\{article\\})", article);
-	    	currentMessage.replaceAll("(\\{link\\})", "minecraft.gamepedia.com/" + article);
-	    	
-	    	return(currentMessage);
-	    			    	    
 	    }
 	    
 	     
